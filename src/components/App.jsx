e8e916a69932b370e68be75f863a797218bd6a6a
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-04-18 15:19:31 +0300

    HW-6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { Section } from './Section/Section';
+import { PhonebookForm } from './PhonebookComponent/Form/Form';
+import { ContactList } from './PhonebookComponent/ContactList/ContactList';
+import { Filter } from './PhonebookComponent/Filter/Filter';
+import { useSelector } from 'react-redux';
+
 export const App = () => {
+  const contacts = useSelector(state => state.contacts.value);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Add new contacts">
+        <PhonebookForm />
+      </Section>
+
+      <Section title="Filter contacts">
+        <Filter />
+      </Section>
+
+      <Section title="Contact List">
+        {contacts.length ? (
+          <ContactList />
+        ) : (
+          <h2 style={{ textAlign: 'center' }}>There is no added contacts</h2>
+        )}
+      </Section>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@emotion/react';
+import { createRoot } from 'react-dom/client';
+import { theme } from './constants/theme';
+import { App } from './components/App';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.querySelector('#root'));
+
+root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </PersistGate>
+    </ThemeProvider>
   </React.StrictMode>
 );
